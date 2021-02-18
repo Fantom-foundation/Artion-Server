0ecfdf1ac94e1ba6090712dfd48eabb4b031d4ba
jason <kwon_jason@yahoo.com>
2021-02-18 05:11:23 +0300

    added authentication

--- apis/auth.js
+++ apis/auth.js
@@ -1,27 +1,156 @@
 const admin = require('firebase-admin')
 const router = require('express').Router()
-const config = require('dotenv').config().parsed
-
-const firebaseConfig = {
-  apiKey: config.APIKEY,
-  authDomain: config.AUTHDOMAIN,
-  projectId: config.PROJECTID,
-  storageBucket: config.STORAGEBUCKET,
-  messagingSenderId: config.MEASSAGINGSENDERID,
-  appId: config.APPID,
-  measurementId: config.MEASUREMENTID,
-}
-
+const emailValidator = require('email-validator')
+const Constants = require('../config/constants')
+const serviceAccount = require('../config/fantomsea_tommy_service.json')
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 })
+const auth = admin.auth()
 
-const db = admin.firestore()
+const checkIfUserExists = async (email) => {
+  try {
+    let user = await auth.getUserByEmail(email)
+    if (user) return true
+    else return false
+  } catch (error) {
+    return false
+  }
+}
+
+const validateEmail = (email) => {
+  return emailValidator.validate(email)
+}
 
 router.get('/test', async (req, res, next) => {
+  console.log(Constants)
   return res.json({
     test: 'test api',
   })
+})
+
+router.post('/addUser', async (req, res, next) => {
+  let name = req.query.name
+  let email = req.query.email
+  let password = req.query.password
+  if (!name)
+    res.status(400).json({
+      msg: Constants.NONAME,
+    })
+  if (!email)
+    res.status(400).json({
+      msg: Constants.NOEMAIL,
+    })
+  if (!validateEmail(email))
+    res.status(400).json({
+      msg: Constants.INVALIDEMAIL,
+    })
+  if (!password)
+    res.status(400).json({
+      msg: Constants.NOPASSWORD,
+    })
+  let isExist = await checkIfUserExists(email)
+  if (isExist)
+    return res.status(400).json({
+      msg: Constants.USEREXISTS,
+    })
+  try {
+    let user = await auth.createUser({
+      displayName: name,
+      email: email,
+      password: password,
+    })
+    if (user)
+      res.json({
+        msg: Constants.USERCREATED,
+      })
+    else
+      res.status(400).json({
+        msg: Constants.USERCREATIONFAILED,
+      })
+  } catch (error) {
+    res.status(400).json({
+      msg: Constants.USERCREATIONFAILED,
+    })
+  }
+})
+
+router.post('/signin', async (req, res, next) => {
+  let email = req.query.email
+
+  try {
+    let user = await auth.getUserByEmail(email)
+    let token = new auth.createCustomToken(user.uid)
+    res.json({
+      msg: Constants.SIGNINJWTTOKEN,
+      token: token,
+    })
+  } catch (error) {
+    res.status(400).json({
+      msg: Constants.SIGNINJWTTOKENFAILED,
+    })
+  }
+})
+
+router.post('/signout', async (req, res, next) => {
+  let email = req.query.email
+
+  try {
+    let user = await auth.getUserByEmail(email)
+    let token = new auth.createCustomToken(user.uid)
+    res.json({
+      msg: Constants.SIGNOUTJWTTOKEN,
+      token: token,
+    })
+  } catch (error) {
+    res.status(400).json({
+      msg: Constants.SIGNOUTJWTTOKENFAILED,
+    })
+  }
+})
+
+router.post('/updateUser', async (req, res, next) => {
+  let name = req.query.name
+  let email = req.query.email
+  let password = req.query.password
+  if (!name)
+    res.status(400).json({
+      msg: Constants.NONAME,
+    })
+  if (!email)
+    res.status(400).json({
+      msg: Constants.NOEMAIL,
+    })
+  if (!validateEmail(email))
+    res.status(400).json({
+      msg: Constants.INVALIDEMAIL,
+    })
+  if (!password)
+    res.status(400).json({
+      msg: Constants.NOPASSWORD,
+    })
+
+  try {
+    let user = await auth.getUserByEmail(email)
+    let uid = user.uid
+    let newUser = await auth.updateUser(uid, {
+      displayName: name,
+      email: email,
+      password: password,
+    })
+    if (newUser)
+      res.json({
+        msg: Constants.USERUPDATED,
+      })
+    else
+      res.status(400).json({
+        msg: Constants.UPDATEUSERFAILED,
+      })
+  } catch (error) {
+    res.status(400).json({
+      msg: Constants.UPDATEUSERFAILED,
+    })
+  }
 })
 
 module.exports = router
---
+++ config/constants.js
@@ -0,0 +1,17 @@
+const Constants = {
+  USEREXISTS: 'user already exists',
+  USERCREATED: 'new user has been created',
+  USERCREATIONFAILED: 'cannot create a new user',
+  INVALIDEMAIL: 'the email address is invalid',
+  NOEMAIL: 'the email address cannot be blank',
+  NONAME: 'user name cannot be blank',
+  NOPASSWORD: 'password cannot be blank',
+  SIGNINJWTTOKEN: 'sign in token has been created',
+  SIGNINJWTTOKENFAILED: 'sign in token creation failed',
+  SIGNOUTJWTTOKEN: 'sign out token has been created',
+  SIGNOUTJWTTOKENFAILED: 'sign out token creation failed',
+  USERUPDATED: 'user has been updated',
+  UPDATEUSERFAILED: 'cannot update a user',
+}
+
+module.exports = Constants
