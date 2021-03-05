e06f93082fb86f884e9dafb3fcd9db764d93f650
tommy <tommychan4241@gmail.com>
2021-03-05 20:26:00 +0800

    done file uploading to server, wip-upload to ipfs

--- apis/index.js
+++ apis/index.js
@@ -1,5 +1,6 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.use('/auth', require('./auth'))
+router.use("/auth", require("./auth"));
+router.use("/ipfs", require("./ipfs"));
 
-module.exports = router
+module.exports = router;
