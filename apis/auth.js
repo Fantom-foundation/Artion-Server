c6e937110099de3a94287f9df244d435aa853e8c
tommy <tommychan4241@gmail.com>
2021-03-19 09:53:33 +0800

    not to use old auth

--- apis/auth.js
+++ apis/auth.js
@@ -1,167 +1,171 @@
-const admin = require("firebase-admin");
+// const admin = require("firebase-admin");
 const router = require("express").Router();
-const emailValidator = require("email-validator");
-const Constants = require("../config/constants");
-const serviceAccount = require("../config/fantomsea_tommy_service.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-const auth = admin.auth();
+// const emailValidator = require("email-validator");
+// const Constants = require("../config/constants");
+// const serviceAccount = require("../config/fantomsea_tommy_service.json");
+// try {
+//   const serviceAccount = require("../config/fantomsea_tommy_service.json");
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// } catch (error) {}
 
-const checkIfUserExists = async (email) => {
-  try {
-    let user = await auth.getUserByEmail(email);
-    if (user) return true;
-    else return false;
-  } catch (error) {
-    return false;
-  }
-};
+// const auth = admin.auth();
 
-const validateEmail = (email) => {
-  return emailValidator.validate(email);
-};
+// const checkIfUserExists = async (email) => {
+//   try {
+//     let user = await auth.getUserByEmail(email);
+//     if (user) return true;
+//     else return false;
+//   } catch (error) {
+//     return false;
+//   }
+// };
 
-router.get("/test", async (req, res, next) => {
-  console.log(Constants);
-  return res.json({
-    test: "test api",
-  });
-});
+// const validateEmail = (email) => {
+//   return emailValidator.validate(email);
+// };
 
-router.post("/addUser", async (req, res, next) => {
-  let email = req.body.email;
-  let password = req.body.password;
-  if (!email)
-    return res.status(400).json({
-      msg: Constants.NOEMAIL,
-    });
-  if (!validateEmail(email))
-    return res.status(400).json({
-      msg: Constants.INVALIDEMAIL,
-    });
-  if (!password)
-    return res.status(400).json({
-      msg: Constants.NOPASSWORD,
-    });
-  let isExist = await checkIfUserExists(email);
-  if (isExist)
-    return res.status(400).json({
-      msg: Constants.USEREXISTS,
-    });
-  try {
-    let user = await auth.createUser({
-      email: email,
-      password: password,
-    });
-    if (user)
-      return res.json({
-        msg: Constants.USERCREATED,
-        user: user,
-      });
-    else
-      return res.status(400).json({
-        msg: Constants.USERCREATIONFAILED,
-      });
-  } catch (error) {
-    return res.status(400).json({
-      msg: Constants.USERCREATIONFAILED,
-    });
-  }
-});
+// router.get("/test", async (req, res, next) => {
+//   console.log(Constants);
+//   return res.json({
+//     test: "test api",
+//   });
+// });
 
-router.post("/signin", async (req, res, next) => {
-  let email = req.body.email;
+// router.post("/addUser", async (req, res, next) => {
+//   let email = req.body.email;
+//   let password = req.body.password;
+//   if (!email)
+//     return res.status(400).json({
+//       msg: Constants.NOEMAIL,
+//     });
+//   if (!validateEmail(email))
+//     return res.status(400).json({
+//       msg: Constants.INVALIDEMAIL,
+//     });
+//   if (!password)
+//     return res.status(400).json({
+//       msg: Constants.NOPASSWORD,
+//     });
+//   let isExist = await checkIfUserExists(email);
+//   if (isExist)
+//     return res.status(400).json({
+//       msg: Constants.USEREXISTS,
+//     });
+//   try {
+//     let user = await auth.createUser({
+//       email: email,
+//       password: password,
+//     });
+//     if (user)
+//       return res.json({
+//         msg: Constants.USERCREATED,
+//         user: user,
+//       });
+//     else
+//       return res.status(400).json({
+//         msg: Constants.USERCREATIONFAILED,
+//       });
+//   } catch (error) {
+//     return res.status(400).json({
+//       msg: Constants.USERCREATIONFAILED,
+//     });
+//   }
+// });
 
-  try {
-    let user = await auth.getUserByEmail(email);
-    let token = new auth.createCustomToken(user.uid);
-    return res.json({
-      msg: Constants.SIGNINJWTTOKEN,
-      token: token,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      msg: Constants.SIGNINJWTTOKENFAILED,
-    });
-  }
-});
+// router.post("/signin", async (req, res, next) => {
+//   let email = req.body.email;
 
-router.post("/signout", async (req, res, next) => {
-  let email = req.body.email;
+//   try {
+//     let user = await auth.getUserByEmail(email);
+//     let token = new auth.createCustomToken(user.uid);
+//     return res.json({
+//       msg: Constants.SIGNINJWTTOKEN,
+//       token: token,
+//     });
+//   } catch (error) {
+//     return res.status(400).json({
+//       msg: Constants.SIGNINJWTTOKENFAILED,
+//     });
+//   }
+// });
 
-  try {
-    let user = await auth.getUserByEmail(email);
-    let token = new auth.createCustomToken(user.uid);
-    return res.json({
-      msg: Constants.SIGNOUTJWTTOKEN,
-      token: token,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      msg: Constants.SIGNOUTJWTTOKENFAILED,
-    });
-  }
-});
+// router.post("/signout", async (req, res, next) => {
+//   let email = req.body.email;
 
-router.post("/updateUser", async (req, res, next) => {
-  let email = req.body.email;
-  let password = req.body.password;
-  if (!email)
-    return res.status(400).json({
-      msg: Constants.NOEMAIL,
-    });
-  if (!validateEmail(email))
-    return res.status(400).json({
-      msg: Constants.INVALIDEMAIL,
-    });
-  if (!password)
-    return res.status(400).json({
-      msg: Constants.NOPASSWORD,
-    });
+//   try {
+//     let user = await auth.getUserByEmail(email);
+//     let token = new auth.createCustomToken(user.uid);
+//     return res.json({
+//       msg: Constants.SIGNOUTJWTTOKEN,
+//       token: token,
+//     });
+//   } catch (error) {
+//     return res.status(400).json({
+//       msg: Constants.SIGNOUTJWTTOKENFAILED,
+//     });
+//   }
+// });
 
-  try {
-    let user = await auth.getUserByEmail(email);
-    let uid = user.uid;
-    let newUser = await auth.updateUser(uid, {
-      email: email,
-      password: password,
-    });
-    if (newUser)
-      return res.json({
-        msg: Constants.USERUPDATED,
-        user: newUser,
-      });
-    else
-      return res.status(400).json({
-        msg: Constants.UPDATEUSERFAILED,
-      });
-  } catch (error) {
-    return res.status(400).json({
-      msg: Constants.UPDATEUSERFAILED,
-    });
-  }
-});
+// router.post("/updateUser", async (req, res, next) => {
+//   let email = req.body.email;
+//   let password = req.body.password;
+//   if (!email)
+//     return res.status(400).json({
+//       msg: Constants.NOEMAIL,
+//     });
+//   if (!validateEmail(email))
+//     return res.status(400).json({
+//       msg: Constants.INVALIDEMAIL,
+//     });
+//   if (!password)
+//     return res.status(400).json({
+//       msg: Constants.NOPASSWORD,
+//     });
 
-router.post("/getUser", async (req, res, next) => {
-  let email = req.body.email;
-  try {
-    let user = await auth.getUserByEmail(email);
-    if (user)
-      return res.json({
-        msg: Constants.USERFOUND,
-        user: user,
-      });
-    else
-      return res.status(400).json({
-        msg: Constants.USERNOTFOUND,
-      });
-  } catch (error) {
-    return res.status(400).json({
-      msg: Constants.USERNOTFOUND,
-    });
-  }
-});
+//   try {
+//     let user = await auth.getUserByEmail(email);
+//     let uid = user.uid;
+//     let newUser = await auth.updateUser(uid, {
+//       email: email,
+//       password: password,
+//     });
+//     if (newUser)
+//       return res.json({
+//         msg: Constants.USERUPDATED,
+//         user: newUser,
+//       });
+//     else
+//       return res.status(400).json({
+//         msg: Constants.UPDATEUSERFAILED,
+//       });
+//   } catch (error) {
+//     return res.status(400).json({
+//       msg: Constants.UPDATEUSERFAILED,
+//     });
+//   }
+// });
 
-router.post("/deleteUser", async (req, res, next) => {});
+// router.post("/getUser", async (req, res, next) => {
+//   let email = req.body.email;
+//   try {
+//     let user = await auth.getUserByEmail(email);
+//     if (user)
+//       return res.json({
+//         msg: Constants.USERFOUND,
+//         user: user,
+//       });
+//     else
+//       return res.status(400).json({
+//         msg: Constants.USERNOTFOUND,
+//       });
+//   } catch (error) {
+//     return res.status(400).json({
+//       msg: Constants.USERNOTFOUND,
+//     });
+//   }
+// });
+
+// router.post("/deleteUser", async (req, res, next) => {});
 module.exports = router;
