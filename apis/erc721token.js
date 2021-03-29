f4615976435c86fb47109403d83bf6caebd9facf
tommy <tommychan4241@gmail.com>
2021-03-29 20:42:59 +0800

    fix erc721 api

--- apis/erc721token.js
+++ apis/erc721token.js
@@ -1,43 +1,63 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
+const formidable = require("formidable");
 const ERC721TOKEN = mongoose.model("ERC721TOKEN");
 
 // save a new token -- returns a json of newly added token
 router.post("/savenewtoken", async (req, res, next) => {
-  let newToken = new ERC721TOKEN();
+  let form = new formidable.IncomingForm();
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        status: "failed",
+      });
+    } else {
+      let contractAddress = fields.contractAddress;
 
-  newToken.contractAddress = req.body.contractAddress;
-  newToken.tokenID = req.body.tokenID;
-  newToken.symbol = req.body.symbol;
-  newToken.royalty = req.body.royalty;
-  newToken.category = req.body.category;
-  newToken.imageHash = req.body.imageHash;
-  newToken.jsonHash = req.body.jsonHash;
-  newToken.createdAt = now;
-  let now = new Date();
+      let newToken = new ERC721TOKEN();
+      newToken.contractAddress = contractAddress;
+      newToken.tokenID = fields.tokenID;
+      newToken.symbol = fields.symbol;
+      newToken.royalty = fields.royalty;
+      newToken.category = fields.category;
+      newToken.imageHash = fields.imageHash;
+      newToken.jsonHash = fields.jsonHash;
+      let now = new Date();
+      newToken.createdAt = now;
 
-  let _newToken = await newToken.save();
-  return res.json({
-    status: "success",
-    data: _newToken.toERC721TOKENJson(),
+      let _newToken = await newToken.save();
+      console.log(_newToken);
+      return res.json({
+        status: "success",
+        data: _newToken.toJSON(),
+      });
+    }
   });
 });
 
 //increase the view count -- returns a increased view count
 
 router.post("/increaseViews", async (req, res, next) => {
-  let contractAddress = req.body.contractAddress;
-  let tokenID = req.body.tokenID;
-  let tokens = ERC721TOKEN.find({
-    contractAddress: contractAddress,
-    tokenID: tokenID,
-  });
-  let token = tokens[0];
-  token.viewed = token.viewed + 1;
-  let _token = await token.save();
-  return res.json({
-    status: "success",
-    data: _token.viewed,
+  let form = new formidable.IncomingForm();
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        status: "failed",
+      });
+    } else {
+      let contractAddress = fields.contractAddress;
+      let tokenID = fields.tokenID;
+      let token = ERC721TOKEN.findOne({
+        contractAddress: contractAddress,
+        tokenID: tokenID,
+      });
+      token.viewed = token.viewed + 1;
+      let _token = await token.save();
+      return res.json({
+        status: "success",
+        data: _token.viewed,
+      });
+    }
   });
 });
 
