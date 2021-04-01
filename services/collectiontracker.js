4031e249e6268abb40218c088df0d3a452c59ab1
tommy <tommychan4241@gmail.com>
2021-04-02 00:44:17 +0800

    fix async issue

--- services/collectiontracker.js
+++ services/collectiontracker.js
@@ -2,29 +2,31 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const contractutils = require("./contract.utils");
 
-require("../models/transferhistory")
+require("../models/transferhistory");
 const TransferHistory = mongoose.model("TransferHistory");
 
 const trackCollectionTransfer = async (address) => {
   let contract = await contractutils.loadContractFromAddress(address);
-  contract.on("Transfer", (from, to, tokenID) => {
-
-    let history = await TransferHistory.findOne({collectionAddress : address,tokenID : tokenID, to : from})
-    if(history){
-        history.from = from
-        history.to = to
-        await token.save()
-    }
-    else{
-        let newHistory = new TransferHistory()
-        newHistory.collectionAddress = address
-        newHistory.from = from
-        newHistory.to = to
-        newHistory.tokenID = tokenID
-        await newHistory.save()
+  contract.on("Transfer", async (from, to, tokenID) => {
+    let history = await TransferHistory.findOne({
+      collectionAddress: address,
+      tokenID: tokenID,
+      to: from,
+    });
+    if (history) {
+      history.from = from;
+      history.to = to;
+      await token.save();
+    } else {
+      let newHistory = new TransferHistory();
+      newHistory.collectionAddress = address;
+      newHistory.from = from;
+      newHistory.to = to;
+      newHistory.tokenID = tokenID;
+      await newHistory.save();
     }
   });
-  return contract
+  return contract;
 };
 
-module.exports = trackCollectionTransfer
+module.exports = trackCollectionTransfer;
