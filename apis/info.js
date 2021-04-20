3ae771ef20192ac69882de74ffef5d43d2b2956d
tommy <tommychan4241@gmail.com>
2021-04-21 02:54:14 +0800

    fix

--- apis/info.js
+++ apis/info.js
@@ -18,12 +18,16 @@ router.get("/getNewestTokens", async (req, res) => {
 router.get("/geterc721contracts", async (req, res) => {
   let all = await ERC721CONTRACT.find({});
   let allCollections = await Collection.find({});
+
+  console.log("all collections are ");
+  console.log(allCollections);
+
   let erc721contracts = new Array();
 
   for (let i = 0; i < all.length; ++i) {
     let contract = all[i];
     let collection = allCollections.find(
-      (col) => col.erc721Address == contract.address
+      (col) => col.erc721Address.toLowerCase() == contract.address.toLowerCase()
     );
     console.log(collection);
 
