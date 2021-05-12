91b71d59775251e68411b45ad9c7c1e0f7c3daa7
tommy <tommychan4241@gmail.com>
2021-05-12 23:16:22 +0800

    update auction model

--- models/auction.js
+++ models/auction.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Auction = mongoose.Schema({
   minter: { type: String, required: true },
   tokenID: { type: Number, required: true },
+  bidder: { type: Number, required: true },
   startTime: { type: Number, default: Date.now },
   endTime: { type: Date, default: Date.now },
 });
