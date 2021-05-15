0a7d57dfbf8198a2c6ff13ca43889ad8b5bb20b7
tommy <tommychan4241@gmail.com>
2021-05-16 01:14:20 +0800

    update trade history api

--- apis/tradehistory.js
+++ apis/tradehistory.js
@@ -5,13 +5,21 @@ const auth = require("./middleware/auth");
 const TradeHistory = mongoose.model("TradeHistory");
 
 router.post("/getTradeHistory", async (req, res) => {
-  let erc721address = req.body.contractAddress;
+  let collectionAddress = req.body.contractAddress;
   let tokenID = req.body.tokenID;
 
   let history = await TradeHistory.find({
-    erc721address: { $regex: new RegExp(erc721address, "i") },
+    collectionAddress: { $regex: new RegExp(collectionAddress, "i") },
     tokenID: tokenID,
-  }).select(["from", "to", "tokenID", "price", "saleDate", "isAuction"]);
+  }).select([
+    "from",
+    "to",
+    "tokenID",
+    "price",
+    "value",
+    "saleDate",
+    "isAuction",
+  ]);
   return res.send({
     status: "success",
     data: history,
