6cb07157d4e0ab432143781619769bd5d3fe7a7a
tommy <tommychan4241@gmail.com>
2021-05-17 22:19:53 +0800

    add save category when 1155 of collection addition

--- apis/collection.js
+++ apis/collection.js
@@ -3,91 +3,94 @@ const { default: axios } = require("axios");
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const Collection = mongoose.model("Collection");
+const Category = mongoose.model("Category");
 const ERC1155CONTRACT = mongoose.model("ERC1155CONTRACT");
 const auth = require("./middleware/auth");
 const toLowerCase = require("../utils/utils");
 const ftmScanApiKey = process.env.FTM_SCAN_API_KEY;
 const isValidERC1155 = require("../utils/1155_validator");
 
-router.post(
-  "/collectiondetails",
-  /*auth,*/ async (req, res) => {
-    let erc721Address = req.body.erc721Address;
-    erc721Address = toLowerCase(erc721Address);
-    let collectionName = req.body.collectionName;
-    let description = req.body.description;
-    let categories = req.body.categories;
-    categories = categories.split(",");
-    let logoImageHash = req.body.logoImageHash;
-    let siteUrl = req.body.siteUrl;
-    let discord = req.body.discord;
-    let twitterHandle = req.body.twitterHandle;
-    let mediumHandle = req.body.mediumHandle;
-    let telegram = req.body.telegram;
-    let instagram = req.body.instagram;
+router.post("/collectiondetails", auth, async (req, res) => {
+  let erc721Address = req.body.erc721Address;
+  erc721Address = toLowerCase(erc721Address);
+  let collectionName = req.body.collectionName;
+  let description = req.body.description;
+  let categories = req.body.categories;
+  categories = categories.split(",");
+  let logoImageHash = req.body.logoImageHash;
+  let siteUrl = req.body.siteUrl;
+  let discord = req.body.discord;
+  let twitterHandle = req.body.twitterHandle;
+  let mediumHandle = req.body.mediumHandle;
+  let telegram = req.body.telegram;
+  let instagram = req.body.instagram;
 
-    let collection = await Collection.findOne({ erc721Address: erc721Address });
-    if (collection) {
-      collection.erc721Address = erc721Address;
-      collection.collectionName = collectionName;
-      collection.description = description;
-      collection.categories = categories;
-      collection.logoImageHash = logoImageHash;
-      collection.siteUrl = siteUrl;
-      collection.discord = discord;
-      collection.twitterHandle = twitterHandle;
-      collection.mediumHandle = mediumHandle;
-      collection.telegram = telegram;
-      collection.instagramHandle = instagram;
+  let collection = await Collection.findOne({ erc721Address: erc721Address });
+  if (collection) {
+    collection.erc721Address = erc721Address;
+    collection.collectionName = collectionName;
+    collection.description = description;
+    collection.categories = categories;
+    collection.logoImageHash = logoImageHash;
+    collection.siteUrl = siteUrl;
+    collection.discord = discord;
+    collection.twitterHandle = twitterHandle;
+    collection.mediumHandle = mediumHandle;
+    collection.telegram = telegram;
+    collection.instagramHandle = instagram;
 
-      let _collection = await collection.save();
-      if (_collection)
-        return res.send({
-          status: "success",
-          data: _collection.toJson(),
-        });
-      else
-        return res.send({
-          status: "failed",
-        });
-    } else {
-      // verify if 1155 smart contracts
-      let is1155 = await isValidERC1155(erc721Address);
-      if (is1155) {
-        // need to add a new 1155 collection
-        let sc_1155 = new ERC1155CONTRACT();
-        sc_1155.address = erc721Address;
-        sc_1155.name = collectionName;
-        sc_1155.symbol = "Symbol";
-        sc_1155.isVerified = true;
-        await sc_1155.save();
-      }
-      // add a new collection
-      let _collection = new Collection();
-      _collection.erc721Address = erc721Address;
-      _collection.collectionName = collectionName;
-      _collection.description = description;
-      _collection.categories = categories;
-      _collection.logoImageHash = logoImageHash;
-      _collection.siteUrl = siteUrl;
-      _collection.discord = discord;
-      _collection.twitterHandle = twitterHandle;
-      _collection.mediumHandle = mediumHandle;
-      _collection.telegram = telegram;
-      _collection.instagramHandle = instagram;
-      let newCollection = await _collection.save();
-      if (newCollection)
-        return res.send({
-          status: "success",
-          data: newCollection.toJson(),
-        });
-      else
-        return res.send({
-          status: "failed",
-        });
+    let _collection = await collection.save();
+    if (_collection)
+      return res.send({
+        status: "success",
+        data: _collection.toJson(),
+      });
+    else
+      return res.send({
+        status: "failed",
+      });
+  } else {
+    // verify if 1155 smart contracts
+    let is1155 = await isValidERC1155(erc721Address);
+    if (is1155) {
+      // need to add a new 1155 collection
+      let sc_1155 = new ERC1155CONTRACT();
+      sc_1155.address = erc721Address;
+      sc_1155.name = collectionName;
+      sc_1155.symbol = "Symbol";
+      sc_1155.isVerified = true;
+      await sc_1155.save();
+      // save new category
+      let category = new Category();
+      category.minterAddress = erc721Address;
+      category.type = 1155;
+      await category.save();
     }
+    // add a new collection
+    let _collection = new Collection();
+    _collection.erc721Address = erc721Address;
+    _collection.collectionName = collectionName;
+    _collection.description = description;
+    _collection.categories = categories;
+    _collection.logoImageHash = logoImageHash;
+    _collection.siteUrl = siteUrl;
+    _collection.discord = discord;
+    _collection.twitterHandle = twitterHandle;
+    _collection.mediumHandle = mediumHandle;
+    _collection.telegram = telegram;
+    _collection.instagramHandle = instagram;
+    let newCollection = await _collection.save();
+    if (newCollection)
+      return res.send({
+        status: "success",
+        data: newCollection.toJson(),
+      });
+    else
+      return res.send({
+        status: "failed",
+      });
   }
-);
+});
 
 router.post("/searchCollection", auth, async (req, res) => {
   let erc721Address = req.body.erc721Address;
