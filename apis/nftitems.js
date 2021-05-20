b2aa704aebcff8d5e4d54158bed17819f13cb386
tommy <tommychan4241@gmail.com>
2021-05-20 15:30:06 +0800

    fix

--- apis/nftitems.js
+++ apis/nftitems.js
@@ -283,6 +283,7 @@ router.post("/fetchTokens", async (req, res) => {
       "tokenURI",
       "price",
       "viewed",
+      "supply",
     ]);
   let allTokens_721_Total = allTokens_721.length;
 
