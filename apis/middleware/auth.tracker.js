692aa9a0a5bc3a7dee88d4ab1927ca9d255763ba
tommy <tommychan4241@gmail.com>
2021-06-23 00:24:32 +0800

    update auth middleware

--- apis/middleware/auth.tracker.js
+++ apis/middleware/auth.tracker.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 const requestIP = require("request-ip");
 
-const tracker_ip_1 = process.env.TRACKER_IP_1;
-const tracker_ip_2 = process.env.TRACKER_IP_2;
+const tracker_ip = process.env.TRACKER_IP;
 
 const service_auth = (req, res, next) => {
   let request_ip = requestIP.getClientIp(req);
-  if (request_ip == tracker_ip_1 || request_ip == tracker_ip_2) next();
+  if (request_ip == tracker_ip) next();
   else
     return res.status(400).json({
       status: "failed",
