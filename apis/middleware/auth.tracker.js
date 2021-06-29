978076b038f6ca8aaf49e7b14e7ab23f1d20f4b5
tommy <tommychan4241@gmail.com>
2021-06-29 21:02:42 +0800

    log

--- apis/middleware/auth.tracker.js
+++ apis/middleware/auth.tracker.js
@@ -5,12 +5,14 @@ const tracker_ip = process.env.TRACKER_IP;
 
 const service_auth = (req, res, next) => {
   let request_ip = requestIP.getClientIp(req);
-  if (request_ip == tracker_ip) next();
-  else
-    return res.status(400).json({
-      status: "failed",
-      data: "you are not supposed to call this api endpoint",
-    });
+  if (request_ip == tracker_ip) {
+    console.log("correct ip");
+    next();
+  } else console.log("invalid ip");
+  return res.status(400).json({
+    status: "failed",
+    data: "you are not supposed to call this api endpoint",
+  });
 };
 
 module.exports = service_auth;
