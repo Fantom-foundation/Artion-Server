8f95a3d9c4c598b2d3cc052485854d6b063009e7
tommy <tommychan4241@gmail.com>
2021-06-30 20:25:52 +0800

    update service auth

--- apis/middleware/auth.tracker.js
+++ apis/middleware/auth.tracker.js
@@ -4,17 +4,14 @@ const requestIP = require("request-ip");
 const tracker_ip = process.env.TRACKER_IP;
 
 const service_auth = (req, res, next) => {
-  next();
-  return;
   let request_ip = requestIP.getClientIp(req);
   if (request_ip == tracker_ip) {
-    console.log("correct ip");
     next();
-  } else console.log("invalid ip");
-  return res.status(400).json({
-    status: "failed",
-    data: "you are not supposed to call this api endpoint",
-  });
+  } else
+    return res.status(400).json({
+      status: "failed",
+      data: "you are not supposed to call this api endpoint",
+    });
 };
 
 module.exports = service_auth;
