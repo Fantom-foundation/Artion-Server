2841febabcfa43785af21bdad9ec89a18f37cda4
tommy <tommychan4241@gmail.com>
2021-07-28 13:15:07 +0800

    add aws mechanical turk related apis with updating current apis

---
+++ apis/moderator.js
@@ -0,0 +1,106 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const Moderator = mongoose.model("Moderator");
+
+const router = require("express").Router();
+const auth = require("./middleware/auth");
+const toLowerCase = require("../utils/utils");
+const extractAddress = require("../services/address.utils");
+const validateSingature = require("../apis/middleware/auth.sign");
+
+const ADMINADDRESS = process.env.ADMINADDRESS;
+
+const isAdmin = (msgSender) => {
+  return toLowerCase(ADMINADDRESS) == toLowerCase(msgSender);
+};
+
+router.post("/add", auth, async (req, res) => {
+  try {
+    let adminAddress = extractAddress(req, res);
+    if (!isAdmin(adminAddress))
+      return res.json({
+        status: "failed",
+        data: "Only Admin can add Mods",
+      });
+    let modAddress = toLowerCase(req.body.address);
+    let modName = req.body.name;
+    let signature = req.body.singature;
+    let isValidSingature = validateSingature(adminAddress, signature);
+    if (!isValidSingature)
+      return res.status(400).json({
+        status: "invalid singature",
+      });
+    let mod = await Moderator.findOne({ address: modAddress });
+    if (mod) {
+      return res.json({
+        status: "failed",
+        data: "Moderator with this address already exists!",
+      });
+    } else {
+      mod = new Moderator();
+      mod.address = modAddress;
+      mod.name = modName;
+      await mod.save();
+      return res.json({
+        status: "success",
+        data: "New Moderator successfully added!",
+      });
+    }
+  } catch (error) {
+    return res.json({
+      status: "failed",
+    });
+  }
+});
+
+router.post("/remove", auth, async (req, res) => {
+  try {
+    let adminAddress = extractAddress(req, res);
+    if (!isAdmin(adminAddress))
+      return res.json({
+        status: "failed",
+        data: "Only Admin can add Mods",
+      });
+    let modAddress = toLowerCase(req.body.address);
+    let signature = req.body.singature;
+    let isValidSingature = validateSingature(adminAddress, signature);
+    if (!isValidSingature)
+      return res.status(400).json({
+        status: "invalid singature",
+      });
+    await Moderator.deleteMany({ address: modAddress });
+    return res.json({
+      status: "success",
+      data: "Successfully removed a moderator",
+    });
+  } catch (error) {
+    return res.json({
+      status: "failed",
+    });
+  }
+});
+
+router.get("/isModerator/:address", async (req, res) => {
+  try {
+    let address = toLowerCase(req.params.address);
+    let mod = await Moderator.findOne({ address: address });
+    if (mod)
+      return res.json({
+        status: "success",
+        data: true,
+      });
+    else
+      return res.json({
+        status: "success",
+        data: false,
+      });
+  } catch (error) {
+    return res.json({
+      status: "failed",
+      data: false,
+    });
+  }
+});
+
+module.exports = router;
