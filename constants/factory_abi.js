195c05e7ef74643a802f1255cb73531e924a0967
Ibrahim Arshad <ibrahim.a.motiwala@gmail.com>
2021-09-17 20:38:39 +0400

    Update New ABIs and addresses

--- constants/factory_abi.js
+++ constants/factory_abi.js
@@ -1,21 +1,21 @@
 const CollectionFactoryContract = {
-  MAINNET_721_PRIVATE: "0xe8d29976368Fc2d8699797faA7cD3684dFf41810",
-  MAINNET_721_PUBLIC: "0x9dA8d9Cc8A7C79e46A3006f55ED98e915b390F5D",
-  TESTNET_721_PRIVATE: "0x130138e2e535304Cce3B3B1B638F54402373391d",
-  TESTNET_721_PUBLIC: "0xa92cBC72eef9254909A3f0eB9E2716eBD28171AE",
-  MAINNET_1155_PRIVATE: "0x8E504d814C0ba86ECD42Ce92273CaFFB3923253B",
-  MAINNET_1155_PUBLIC: "0xe25575E99AE02D8C798d63Edd6aeB5C520f1c5b8",
-  TESTNET_1155_PRIVATE: "0x130138e2e535304Cce3B3B1B638F54402373391d",
-  TESTNET_1155_PUBLIC: "0xa92cBC72eef9254909A3f0eB9E2716eBD28171AE",
+  MAINNET_721_PRIVATE: '0xe5841838Dd7e522f217DfFBCEaef82F04EC649Cd', //FantomNFTFactoryPrivate
+  MAINNET_721_PUBLIC: '0xCC7A2eC7A8A0564518fD3D2ca0Df8B2137626144', //FantomNFTFactory
+  TESTNET_721_PRIVATE: '0x49191888c75134E60889c5407A87Cf07F836f677', //FantomNFTFactoryPrivate
+  TESTNET_721_PUBLIC: '0x6F124f6DABA769Eb351a1EeC4C0224F9A0a524cE', //FantomNFTFactory
+  MAINNET_1155_PRIVATE: '0x736Eae40AdFf88570b92378c97a0D11b44E1C953', //FantomArtFactoryPrivate
+  MAINNET_1155_PUBLIC: '0x520DaB621f93F59d3557174280AB1B6d4FB8c956', //FantomArtFactory
+  TESTNET_1155_PRIVATE: '0x736Eae40AdFf88570b92378c97a0D11b44E1C953', //FantomArtFactoryPrivate
+  TESTNET_1155_PUBLIC: '0x520DaB621f93F59d3557174280AB1B6d4FB8c956', //FantomArtFactory
   ABI: [
     {
-      inputs: [{ internalType: "address", name: "", type: "address" }],
-      name: "exists",
-      outputs: [{ internalType: "bool", name: "", type: "bool" }],
-      stateMutability: "view",
-      type: "function",
-    },
-  ],
+      inputs: [{ internalType: 'address', name: '', type: 'address' }],
+      name: 'exists',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'view',
+      type: 'function'
+    }
+  ]
 };
 
 module.exports = CollectionFactoryContract;
--- constants/paytokenRegistry_abi.js
+++ constants/paytokenRegistry_abi.js
@@ -1,7 +1,7 @@
 const PaytokenRegistryABI = {
   address: {
-    mainnet: "0x46410532E88FCA08483A6757f9496186dC479088",
-    testnet: "0x550304777B3e5bf3205DbCBD9b3EB21E202eFbdE",
+    mainnet: '0xF8f84d3c73c0836801A2a6d616135318822a0E49',
+    testnet: '0xAb4a004Bef04f16C93Cc407A58e316B65d7dF0C7'
   },
   abi: [
     {
@@ -9,89 +9,89 @@ const PaytokenRegistryABI = {
       inputs: [
         {
           indexed: true,
-          internalType: "address",
-          name: "previousOwner",
-          type: "address",
+          internalType: 'address',
+          name: 'previousOwner',
+          type: 'address'
         },
         {
           indexed: true,
-          internalType: "address",
-          name: "newOwner",
-          type: "address",
-        },
+          internalType: 'address',
+          name: 'newOwner',
+          type: 'address'
+        }
       ],
-      name: "OwnershipTransferred",
-      type: "event",
+      name: 'OwnershipTransferred',
+      type: 'event'
     },
     {
       anonymous: false,
       inputs: [
         {
           indexed: false,
-          internalType: "address",
-          name: "token",
-          type: "address",
-        },
+          internalType: 'address',
+          name: 'token',
+          type: 'address'
+        }
       ],
-      name: "TokenAdded",
-      type: "event",
+      name: 'TokenAdded',
+      type: 'event'
     },
     {
       anonymous: false,
       inputs: [
         {
           indexed: false,
-          internalType: "address",
-          name: "token",
-          type: "address",
-        },
+          internalType: 'address',
+          name: 'token',
+          type: 'address'
+        }
       ],
-      name: "TokenRemoved",
-      type: "event",
+      name: 'TokenRemoved',
+      type: 'event'
     },
     {
-      inputs: [{ internalType: "address", name: "token", type: "address" }],
-      name: "add",
+      inputs: [{ internalType: 'address', name: 'token', type: 'address' }],
+      name: 'add',
       outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
+      stateMutability: 'nonpayable',
+      type: 'function'
     },
     {
-      inputs: [{ internalType: "address", name: "", type: "address" }],
-      name: "enabled",
-      outputs: [{ internalType: "bool", name: "", type: "bool" }],
-      stateMutability: "view",
-      type: "function",
+      inputs: [{ internalType: 'address', name: '', type: 'address' }],
+      name: 'enabled',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'view',
+      type: 'function'
     },
     {
       inputs: [],
-      name: "owner",
-      outputs: [{ internalType: "address", name: "", type: "address" }],
-      stateMutability: "view",
-      type: "function",
+      name: 'owner',
+      outputs: [{ internalType: 'address', name: '', type: 'address' }],
+      stateMutability: 'view',
+      type: 'function'
     },
     {
-      inputs: [{ internalType: "address", name: "token", type: "address" }],
-      name: "remove",
+      inputs: [{ internalType: 'address', name: 'token', type: 'address' }],
+      name: 'remove',
       outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
+      stateMutability: 'nonpayable',
+      type: 'function'
     },
     {
       inputs: [],
-      name: "renounceOwnership",
+      name: 'renounceOwnership',
       outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
+      stateMutability: 'nonpayable',
+      type: 'function'
     },
     {
-      inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
-      name: "transferOwnership",
+      inputs: [{ internalType: 'address', name: 'newOwner', type: 'address' }],
+      name: 'transferOwnership',
       outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-  ],
+      stateMutability: 'nonpayable',
+      type: 'function'
+    }
+  ]
 };
 
 module.exports = PaytokenRegistryABI;
