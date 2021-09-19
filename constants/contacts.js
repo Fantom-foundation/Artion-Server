3fb22707b83b3198533afff17badce7c4bf438b1
javamusat <javamusat@gmail.com>
2021-09-19 11:31:24 +0200

    [contact] remove fantom instagram

--- constants/contacts.js
+++ constants/contacts.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const FantomContacts = {
   discord: "http://chat.fantom.network/",
   twitter: "https://twitter.com/FantomFDN",
-  instagram: "https://www.instagram.com/official.fantom.foundation/",
   telegram: "https://t.me/fantomfoundation",
   reddit: "https://reddit.com/r/FantomFoundation",
   artionUnsubscribe: `https://${
