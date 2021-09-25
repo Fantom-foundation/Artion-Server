d0104bf10a7d3afc4a492e976abafbd3c92c511a
Ibrahim Arshad <ibrahim.a.motiwala@gmail.com>
2021-09-25 19:22:02 +0400

    Update banItem APIs to remove banned items

--- apis/ban.js
+++ apis/ban.js
@@ -1,23 +1,23 @@
-require("dotenv").config();
-const router = require("express").Router();
+require('dotenv').config();
+const router = require('express').Router();
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const BannedUser = mongoose.model("BannedUser");
-const ERC721CONTRACT = mongoose.model("ERC721CONTRACT");
-const ERC1155CONTRACT = mongoose.model("ERC1155CONTRACT");
-const Collection = mongoose.model("Collection");
-const NFTITEM = mongoose.model("NFTITEM");
-const Moderator = mongoose.model("Moderator");
-const TurkWork = mongoose.model("TurkWork");
+const BannedUser = mongoose.model('BannedUser');
+const ERC721CONTRACT = mongoose.model('ERC721CONTRACT');
+const ERC1155CONTRACT = mongoose.model('ERC1155CONTRACT');
+const Collection = mongoose.model('Collection');
+const NFTITEM = mongoose.model('NFTITEM');
+const Moderator = mongoose.model('Moderator');
+const TurkWork = mongoose.model('TurkWork');
 
-const auth = require("./middleware/auth");
-const toLowerCase = require("../utils/utils");
-const validateSignature = require("../apis/middleware/auth.sign");
+const auth = require('./middleware/auth');
+const toLowerCase = require('../utils/utils');
+const validateSignature = require('../apis/middleware/auth.sign');
 
 const adminAddress = process.env.ADMINADDRESS;
 
-const extractAddress = require("../services/address.utils");
+const extractAddress = require('../services/address.utils');
 
 const isAdmin = (msgSender) => {
   return toLowerCase(adminAddress) == toLowerCase(msgSender);
@@ -31,7 +31,7 @@ const canBanNFT = async (address) => {
   else return false;
 };
 
-router.post("/banUser", auth, async (req, res) => {
+router.post('/banUser', auth, async (req, res) => {
   try {
     let adminAddress = extractAddress(req, res);
     if (isAdmin(adminAddress)) {
@@ -40,39 +40,39 @@ router.post("/banUser", auth, async (req, res) => {
       try {
         let bannedUser = new BannedUser();
         bannedUser.address = banAddress;
-        bannedUser.reason = reason ? reason : "";
+        bannedUser.reason = reason ? reason : '';
         await bannedUser.save();
         return res.json({
-          status: "success",
-          data: "banned",
+          status: 'success',
+          data: 'banned'
         });
       } catch (error) {
         return res.json({
-          status: "failed",
-          data: "user is alread banned",
+          status: 'failed',
+          data: 'user is alread banned'
         });
       }
     } else {
       return res.json({
-        status: "failed",
-        data: "You are not an admin",
+        status: 'failed',
+        data: 'You are not an admin'
       });
     }
   } catch (error) {
     return res.status(400).json({
-      status: "failed",
+      status: 'failed'
     });
   }
 });
 
-router.post("/banItem", auth, async (req, res) => {
+router.post('/banItem', auth, async (req, res) => {
   try {
     let adminAddress = extractAddress(req, res);
     let isModOrAdmin = await canBanNFT(adminAddress);
     if (!isModOrAdmin)
       return res.json({
-        status: "failed",
-        data: "Only Admin or Mods can ban NFT!",
+        status: 'failed',
+        data: 'Only Admin or Mods can ban NFT!'
       });
     let signature = req.body.signature;
     let retrievedAddr = req.body.signatureAddress;
@@ -83,60 +83,34 @@ router.post("/banItem", auth, async (req, res) => {
     );
     if (!isValidsignature)
       return res.json({
-        status: "failed",
-        data: "Invalid Signature",
+        status: 'failed',
+        data: 'Invalid Signature'
       });
     let address = toLowerCase(req.body.address);
     let tokenID = parseInt(req.body.tokenID);
     try {
-      let nft = NFTITEM.findOne({
-        contractAddress: address,
-        tokenID: tokenID,
-      });
-      if (!nft)
-        return res.json({
-          status: "failed",
-          data: "This Item doesn't exist",
-        });
-      if (nft.isAppropriate) {
-        nft.isAppropriate = false;
-        await nft.save();
-        let turkWork = new TurkWork();
-        turkWork.contractAddress = contractAddress;
-        turkWork.tokenID = tokenID;
-        turkWork.banDate = new Date();
-        await turkWork.save();
-        return res.json({
-          status: "success",
-          data: "banned",
-        });
-      } else {
-        return res.json({
-          status: "failed",
-          data: "This Item is already banned",
-        });
-      }
+      await NFTITEM.deleteOne({ contractAddress: address, tokenID: tokenID });
     } catch (error) {
       return res.json({
-        status: "failed",
-        data: "This Item is already banned",
+        status: 'failed',
+        data: 'This Item does not exist!'
       });
     }
   } catch (error) {
     return res.status(400).json({
-      status: "failed",
+      status: 'failed'
     });
   }
 });
 
-router.post("/banCollection", auth, async (req, res) => {
+router.post('/banCollection', auth, async (req, res) => {
   try {
     let adminAddress = extractAddress(req, res);
     let isModOrAdmin = await canBanNFT(adminAddress);
     if (!isModOrAdmin)
       return res.json({
-        status: "failed",
-        data: "Only Admin or Mods can ban Collections!",
+        status: 'failed',
+        data: 'Only Admin or Mods can ban Collections!'
       });
     let signature = req.body.signature;
     let retrievedAddr = req.body.signatureAddress;
@@ -147,8 +121,8 @@ router.post("/banCollection", auth, async (req, res) => {
     );
     if (!isValidsignature)
       return res.json({
-        status: "failed",
-        data: "Invalid Signature",
+        status: 'failed',
+        data: 'Invalid Signature'
       });
 
     let contractAddress = toLowerCase(req.body.address);
@@ -178,36 +152,36 @@ router.post("/banCollection", auth, async (req, res) => {
     } catch (error) {}
 
     let collectionItems = await NFTITEM.find({
-      contractAddress: contractAddress,
+      contractAddress: contractAddress
     });
     let worksData = [];
     collectionItems.map((item) => {
       worksData.push({
         contractAddress: contractAddress,
         tokenID: item.tokenID,
-        banDate: new Date(),
+        banDate: new Date()
       });
     });
     await TurkWork.insertMany(worksData);
     return res.json({
-      status: "success",
-      data: "banned",
+      status: 'success',
+      data: 'banned'
     });
   } catch (error) {
     console.log(error);
     return res.json({
-      status: "Failed to ban a collection!",
+      status: 'Failed to ban a collection!'
     });
   }
 });
 
-router.post("/unbanCollection", auth, async (req, res) => {
+router.post('/unbanCollection', auth, async (req, res) => {
   try {
     let adminAddress = extractAddress(req, res);
     if (!isAdmin(adminAddress))
       return res.json({
-        status: "failed",
-        data: "Only Admin can unban collection!",
+        status: 'failed',
+        data: 'Only Admin can unban collection!'
       });
     let signature = req.body.signature;
     let retrievedAddr = req.body.signatureAddress;
@@ -218,8 +192,8 @@ router.post("/unbanCollection", auth, async (req, res) => {
     );
     if (!isValidsignature)
       return res.json({
-        status: "failed",
-        data: "Invalid Signature",
+        status: 'failed',
+        data: 'Invalid Signature'
       });
 
     let contractAddress = toLowerCase(req.body.address);
@@ -247,25 +221,25 @@ router.post("/unbanCollection", auth, async (req, res) => {
       );
     } catch (error) {}
     return res.json({
-      status: "success",
-      data: "unbanned",
+      status: 'success',
+      data: 'unbanned'
     });
   } catch (error) {
     console.log(error);
     return res.json({
-      status: "Failed to unban a collection!",
+      status: 'Failed to unban a collection!'
     });
   }
 });
 
-router.post("/banItems", auth, async (req, res) => {
+router.post('/banItems', auth, async (req, res) => {
   try {
     let adminAddress = extractAddress(req, res);
     let isModOrAdmin = await canBanNFT(adminAddress);
     if (!isModOrAdmin)
       return res.json({
-        status: "failed",
-        data: "Only Admin or Mods can ban NFT!",
+        status: 'failed',
+        data: 'Only Admin or Mods can ban NFT!'
       });
     let signature = req.body.signature;
     let retrievedAddr = req.body.signatureAddress;
@@ -276,79 +250,64 @@ router.post("/banItems", auth, async (req, res) => {
     );
     if (!isValidsignature)
       return res.json({
-        status: "failed",
-        data: "Invalid Signature",
+        status: 'failed',
+        data: 'Invalid Signature'
       });
 
     let contractAddress = toLowerCase(req.body.address);
     let _tokenIDs = req.body.tokenIDs;
-    _tokenIDs = _tokenIDs.split(",");
+    _tokenIDs = _tokenIDs.split(',');
     let tokenIDs = [];
     _tokenIDs.map((tkID) => {
       tokenIDs.push(parseInt(tkID));
     });
-    await NFTITEM.updateMany(
-      {
-        contractAddress: contractAddress,
-        tokenID: { $in: tokenIDs },
-      },
-      { $set: { isAppropriate: false } }
-    );
-    let worksData = [];
-    tokenIDs.map((tkID) => {
-      worksData.push({
-        contractAddress: contractAddress,
-        tokenID: tkID,
-        banDate: new Date(),
-      });
-    });
-    await TurkWork.insertMany(worksData);
-    return res.json({
-      status: "success",
-      data: "banned",
+
+    await NFTITEM.deleteMany({
+      contractAddress: contractAddress,
+      tokenID: { $in: tokenIDs }
     });
   } catch (error) {
     console.log(error);
     return res.json({
-      status: "Failed to ban NFT Items!",
+      status: 'Failed to ban NFT Items!'
     });
   }
 });
 
-router.post("/boostCollection", auth, async (req, res) => {
+router.post('/boostCollection', auth, async (req, res) => {
   try {
     let adminAddress = extractAddress(req, res);
     if (isAdmin(adminAddress)) {
       let address = toLowerCase(req.body.address);
       try {
         let contract = await ERC721CONTRACT.findOne({
-          address: address,
+          address: address
         });
         if (contract) {
           contract.isVerified = true;
           await contract.save();
           return res.json({
-            status: "success",
-            data: "collection boosted",
+            status: 'success',
+            data: 'collection boosted'
           });
         } else {
           return res.status(400).json({
-            status: "failed",
+            status: 'failed'
           });
         }
       } catch (error) {
         return res.status(400).json({
-          status: "failed",
+          status: 'failed'
         });
       }
     } else {
       return res.status(400).json({
-        status: "failed",
+        status: 'failed'
       });
     }
   } catch (error) {
     return res.status(400).json({
-      status: "failed",
+      status: 'failed'
     });
   }
 });
