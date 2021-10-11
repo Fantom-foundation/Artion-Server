46ebaa560cdc2f5762a9570bba6f9d03b3539629
Ibrahim Arshad <ibrahim.a.motiwala@gmail.com>
2021-10-11 11:32:10 +0400

    Update ABIs (auction and marketplace)

--- constants/auctionabi.js
+++ constants/auctionabi.js
@@ -240,31 +240,6 @@ const AuctionContractABI = [
       },
       {
         indexed: false,
-        internalType: 'uint256',
-        name: 'endTime',
-        type: 'uint256'
-      }
-    ],
-    name: 'UpdateAuctionEndTime',
-    type: 'event'
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'nftAddress',
-        type: 'address'
-      },
-      {
-        indexed: true,
-        internalType: 'uint256',
-        name: 'tokenId',
-        type: 'uint256'
-      },
-      {
-        indexed: false,
         internalType: 'address',
         name: 'payToken',
         type: 'address'
@@ -283,31 +258,6 @@ const AuctionContractABI = [
     anonymous: false,
     inputs: [
       {
-        indexed: true,
-        internalType: 'address',
-        name: 'nftAddress',
-        type: 'address'
-      },
-      {
-        indexed: true,
-        internalType: 'uint256',
-        name: 'tokenId',
-        type: 'uint256'
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'startTime',
-        type: 'uint256'
-      }
-    ],
-    name: 'UpdateAuctionStartTime',
-    type: 'event'
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
         indexed: false,
         internalType: 'uint256',
         name: 'bidWithdrawalLockTime',
@@ -315,19 +265,6 @@ const AuctionContractABI = [
       }
     ],
     name: 'UpdateBidWithdrawalLockTime',
-    type: 'event'
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'minBidIncrement',
-        type: 'uint256'
-      }
-    ],
-    name: 'UpdateMinBidIncrement',
     type: 'event'
   },
   {
@@ -393,6 +330,11 @@ const AuctionContractABI = [
         internalType: 'address',
         name: 'payToken',
         type: 'address'
+      },
+      {
+        internalType: 'uint256',
+        name: 'minBid',
+        type: 'uint256'
       },
       {
         internalType: 'uint256',
@@ -475,6 +417,11 @@ const AuctionContractABI = [
         internalType: 'uint256',
         name: '_startTimestamp',
         type: 'uint256'
+      },
+      {
+        internalType: 'bool',
+        name: 'minBidReserve',
+        type: 'bool'
       },
       {
         internalType: 'uint256',
@@ -632,7 +579,7 @@ const AuctionContractABI = [
   },
   {
     inputs: [],
-    name: 'minBidIncrement',
+    name: 'maxAuctionLength',
     outputs: [
       {
         internalType: 'uint256',
@@ -762,6 +709,24 @@ const AuctionContractABI = [
     type: 'function'
   },
   {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_nftAddress',
+        type: 'address'
+      },
+      {
+        internalType: 'uint256',
+        name: '_tokenId',
+        type: 'uint256'
+      }
+    ],
+    name: 'resultFailedAuction',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
     inputs: [],
     name: 'toggleIsPaused',
     outputs: [],
@@ -808,57 +773,11 @@ const AuctionContractABI = [
       },
       {
         internalType: 'uint256',
-        name: '_endTimestamp',
-        type: 'uint256'
-      }
-    ],
-    name: 'updateAuctionEndTime',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: '_nftAddress',
-        type: 'address'
-      },
-      {
-        internalType: 'uint256',
-        name: '_tokenId',
-        type: 'uint256'
-      },
-      {
-        internalType: 'uint256',
         name: '_reservePrice',
         type: 'uint256'
       }
     ],
     name: 'updateAuctionReservePrice',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: '_nftAddress',
-        type: 'address'
-      },
-      {
-        internalType: 'uint256',
-        name: '_tokenId',
-        type: 'uint256'
-      },
-      {
-        internalType: 'uint256',
-        name: '_startTime',
-        type: 'uint256'
-      }
-    ],
-    name: 'updateAuctionStartTime',
     outputs: [],
     stateMutability: 'nonpayable',
     type: 'function'
@@ -872,19 +791,6 @@ const AuctionContractABI = [
       }
     ],
     name: 'updateBidWithdrawalLockTime',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '_minBidIncrement',
-        type: 'uint256'
-      }
-    ],
-    name: 'updateMinBidIncrement',
     outputs: [],
     stateMutability: 'nonpayable',
     type: 'function'
@@ -911,24 +817,6 @@ const AuctionContractABI = [
       }
     ],
     name: 'updatePlatformFeeRecipient',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
-  },
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: '_nftAddress',
-        type: 'address'
-      },
-      {
-        internalType: 'uint256',
-        name: '_tokenId',
-        type: 'uint256'
-      }
-    ],
-    name: 'withdrawBid',
     outputs: [],
     stateMutability: 'nonpayable',
     type: 'function'
