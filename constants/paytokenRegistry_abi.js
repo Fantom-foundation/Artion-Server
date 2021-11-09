5840fcb2ce3601916c6cb47c2ecbc867c96e8a05
Ibrahim Arshad <ibrahim.a.motiwala@gmail.com>
2021-11-09 15:19:56 +0400

    Update testnet addresses

--- constants/paytokenRegistry_abi.js
+++ constants/paytokenRegistry_abi.js
@@ -1,7 +1,7 @@
 const PaytokenRegistryABI = {
   address: {
     mainnet: '0xF8f84d3c73c0836801A2a6d616135318822a0E49',
-    testnet: '0xAb4a004Bef04f16C93Cc407A58e316B65d7dF0C7'
+    testnet: '0x81Ed1Fb42D26b1813c5E3a43874d3485a42F8692'
   },
   abi: [
     {
